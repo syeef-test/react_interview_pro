002f755e203bb74535e047c39c089f15d505a979
syeef <syeefislam@yahoo.com>
2024-03-12 18:07:53 +0530

    basic accordian created

---
+++ src/components/accordian/data.jsx
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    question: "Some random question ?",
+    answer: "Test answer.",
+  },
+  {
+    id: 2,
+    question: "Some random question ?",
+    answer: "Test answer.",
+  },
+  {
+    id: 3,
+    question: "Some random question ?",
+    answer: "Test answer.",
+  },
+  {
+    id: 4,
+    question: "Some random question ?",
+    answer: "Test answer.",
+  },
+];
+
+export default data;
